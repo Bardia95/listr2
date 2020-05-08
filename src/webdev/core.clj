c199d23fbcc2a253d89d8369e9059606c4f59b58
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-08 18:54:13 -0400

    Added about route and handler :

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -4,27 +4,29 @@
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]))
 
-
 (defn greet [req]
   {:status 200
    :body "Hello , World!"
    :headers {}})
 
 (defn goodbye [req]
-{:status 200
-:body "Goodbye, cruel world!"
-:headers {}})
+  {:status 200
+   :body "Goodbye, cruel world!"
+   :headers {}})
 
+(defn about [req]
+  {:status 200
+   :body "My name is Bardia and this is an app I'm building to learn how to code web apps in Clojure"
+   :headers {}})
 
 (defroutes app
-(GET "/" [] greet)
-(GET "/goodbye" [] goodbye)
-(not-found "Page not found."))
-
+  (GET "/" [] greet)
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (not-found "Page not found."))
 
 (defn -main [port]
-(jetty/run-jetty app {:port (Integer. port)}))
-
+  (jetty/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main [port]
-(jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
