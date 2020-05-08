d5f9cb2cf301c39799c0c6381324fcb9caaf6012
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-08 18:51:16 -0400

    Refactored greet function

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -6,36 +6,25 @@
 
 
 (defn greet [req]
-  (cond
-    (= "/" (req :uri))
-    {:status 200
-     :body "Hello , World!"
-     :headers {}}
-    (= "/goodbye" (req :uri))
-    {:status 200
-     :body "Goodbye"
-     :headers {}}
-    :else
-    {:status 404
-     :body "Not found"
-     :headers {}}))
-
+  {:status 200
+   :body "Hello , World!"
+   :headers {}})
 
 (defn goodbye [req]
-  {:status 200
-   :body "Goodbye, cruel world!"
-   :headers {}})
+{:status 200
+:body "Goodbye, cruel world!"
+:headers {}})
 
 
 (defroutes app
-  (GET "/" [] greet)
-  (GET "/goodbye" [] goodbye)
-  (not-found "Page not found."))
+(GET "/" [] greet)
+(GET "/goodbye" [] goodbye)
+(not-found "Page not found."))
 
 
 (defn -main [port]
-  (jetty/run-jetty app {:port (Integer. port)}))
+(jetty/run-jetty app {:port (Integer. port)}))
 
 
 (defn -dev-main [port]
-  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
+(jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
