9a8fed38d93019dbde7e7225eca95e40f80b061d
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-05-09 00:24:08 -0400

    Added psql support + data models for todo items

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.8.1"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.postgresql/postgresql "42.2.12"]]
   :min-lein-version "2.0.0"
   :uberjar-name "webdev.jar"
   :repl-options {:init-ns webdev.core}
